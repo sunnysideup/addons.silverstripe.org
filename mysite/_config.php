fc524d916bcda67fe699531b993009cb07d20fe2
Nicolaas <github@sunnysideup.co.nz>
2016-12-25 21:26:26 +1300

    PATCH: adding theme

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,9 +3,11 @@
 global $project;
 $project = 'mysite';
 
+SSViewer::set_theme('ssu');
+
 global $database;
 if (!defined('SS_DATABASE_NAME')) {
-	$database = 'addons';
+    $database = 'addons';
 }
 
 require_once 'conf/ConfigureFromEnv.php';
