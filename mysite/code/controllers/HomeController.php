c4ea0ecbfe74a2e58d18c829af73c612f92a8ecd
Nicolaas <github@sunnysideup.co.nz>
2016-12-25 21:27:20 +1300

    PATCH: removing graph

--- mysite/code/controllers/HomeController.php
+++ mysite/code/controllers/HomeController.php
@@ -4,83 +4,50 @@
  */
 class HomeController extends SiteController {
 
-	private static $popular_blacklist = array(
-		'silverstripe/framework',
-		'silverstripe/cms',
-		'silverstripe/sqlite3',
-		'silverstripe/postgresql',
-		'silverstripe-themes/simple'
-	);
+    private static $popular_blacklist = array(
+        'silverstripe/framework',
+        'silverstripe/cms',
+        'silverstripe/sqlite3',
+        'silverstripe/postgresql',
+        'silverstripe-themes/simple'
+    );
 
-	public static $allowed_actions = array(
-		'index'
-	);
+    public static $allowed_actions = array(
+        'index'
+    );
 
-	public function index() {
-		return $this->renderWith(array('Home', 'Page'));
-	}
+    public function index() {
+        return $this->renderWith(array('Home', 'Page'));
+    }
 
-	public function Title() {
-		return 'Home';
-	}
+    public function Title() {
+        return 'Home';
+    }
 
-	public function Link() {
-		return Director::baseURL();
-	}
+    public function Link() {
+        return Director::baseURL();
+    }
 
-	public function PopularAddons($limit = 10) {
-		return Addon::get()
-			->sort('Downloads', 'DESC')
-			->exclude('Name', $this->config()->popular_blacklist)
-			->limit($limit);
-	}
+    public function PopularAddons($limit = 10) {
+        return Addon::get()
+            ->sort('Downloads', 'DESC')
+            ->exclude('Name', $this->config()->popular_blacklist)
+            ->limit($limit);
+    }
 
-	public function NewestAddons($limit = 10) {
-		return Addon::get()->sort('Released', 'DESC')->limit($limit);
-	}
+    public function NewestAddons($limit = 10) {
+        return Addon::get()->sort('Released', 'DESC')->limit($limit);
+    }
 
-	public function RandomAddons($limit = 10) {
-		return Addon::get()->sort(DB::getConn()->random(), 'DESC')->limit($limit);
-	}
+    public function RandomAddons($limit = 10) {
+        return Addon::get()->sort(DB::getConn()->random(), 'DESC')->limit($limit);
+    }
 
-	public function NewestVersions($limit = 10) {
-		return AddonVersion::get()
-			->filter('Development', false)
-			->sort('Released', 'DESC')
-			->limit($limit);
-	}
+    public function NewestVersions($limit = 10) {
+        return AddonVersion::get()
+            ->filter('Development', false)
+            ->sort('Released', 'DESC')
+            ->limit($limit);
+    }
 
-	public function ChartData() {
-		$chartData = array();
-		$list = ArrayList::create(array());
-
-		$sqlQuery = new SQLQuery();
-		$sqlQuery->setFrom('Addon');
-		$sqlQuery->setSelect('DATE(Created) as Created');
-		$sqlQuery->selectField('COUNT(*)', 'CountInOneDay');
-		$sqlQuery->addWhere('"Created" >= DATE_SUB(NOW(), INTERVAL 30 DAY)');
-		$sqlQuery->addGroupBy('DATE(Created)');
-
-		$result = $sqlQuery->execute();
-
-		if(count($result)) {
-			foreach($result as $row) {
-				$date = date('j M Y', strtotime($row['Created']));
-				if(!isset($chartData[$date])) {
-					$chartData[$date] = $row['CountInOneDay'];
-				}
-			}
-		}
-
-		if(count($chartData)) {
-			foreach($chartData as $x => $y) {
-				$list->push(ArrayData::create(array(
-					'XValue' => $x,
-					'YValue' => $y
-				)));
-			}
-		}
-
-		return $list;
-	}
 }
