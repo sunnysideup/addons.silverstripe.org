e10cc5d742fca4c705612cb23aeca29a8cfcd1e2
Nicolaas <github@sunnysideup.co.nz>
2017-01-04 10:07:37 +1300

    PATH: allowing the packages to be limited

--- mysite/code/services/PackagistService.php
+++ mysite/code/services/PackagistService.php
@@ -11,99 +11,107 @@ use Guzzle\Http\Client;
  */
 class PackagistService {
 
-	/**
-	 * @var Composer\Composer
-	 */
-	private $composer;
+    /**
+     * @var Composer\Composer
+     */
+    private $composer;
 
-	/**
-	 * @var Composer\Repository\RepositoryInterface
-	 */
-	private $repository;
+    /**
+     * @var Composer\Repository\RepositoryInterface
+     */
+    private $repository;
 
-	public function __construct() {
-		$this->composer = Factory::create(new NullIO());
-		$this->client = new Packagist\Api\Client();
-	}
+    public function __construct() {
+        $this->composer = Factory::create(new NullIO());
+        $this->client = new Packagist\Api\Client();
+    }
 
-	/**
-	 * @return Composer\Composer
-	 */
-	public function getComposer() {
-		return $this->composer;
-	}
+    /**
+     * @return Composer\Composer
+     */
+    public function getComposer() {
+        return $this->composer;
+    }
 
-	/**
-	 * Gets all SilverStripe packages.
-	 *
-	 * @return Packagist\Api\Package[]
-	 */
-	public function getPackages() {
-		$packages = array();
-		$loader = new ArrayLoader();
+    /**
+     * Gets all SilverStripe packages.
+     * you can limit it to one Vendor
+     * with the limitAddons variable
 
-		$addonTypes = array(
-			'silverstripe-module',
-			'silverstripe-theme'
-		);
-		foreach ($addonTypes as $type) {
-			$repositoriesNames = $this->client->all(array('type' => $type));
-			foreach ($repositoriesNames as $name) {
-				$packages[] = $this->client->get($name);
-				echo $name . PHP_EOL; //output to give feedback when running
-			}
-		}
-		return $packages;
-	}
+     * @param string (optional) $limitAddons
+     * @return Packagist\Api\Package[]
+     */
+    public function getPackages($limitAddons = '') {
+        $packages = array();
+        $loader = new ArrayLoader();
 
-	/**
-	 * Gets all SilverStripe packages, grouped by package name.
-	 *
-	 * @return array
-	 */
-	public function getGroupedPackages() {
-		$grouped = array();
+        $addonTypes = array(
+            'silverstripe-module',
+            'silverstripe-theme'
+        );
+        foreach ($addonTypes as $type) {
+            if($limitAddons) {
+                $repositoriesNames = $this->client->all(array('vendor' => $limitAddons, 'type' => $type));
+            }
+            else {
+                $repositoriesNames = $this->client->all(array('type' => $type));
+            }
+            foreach ($repositoriesNames as $name) {
+                $packages[] = $this->client->get($name);
+                echo $name . PHP_EOL; //output to give feedback when running
+            }
+        }
+        return $packages;
+    }
 
-		foreach ($this->getPackages() as $package) {
-			$name = $package->getName();
+    /**
+     * Gets all SilverStripe packages, grouped by package name.
+     *
+     * @return array
+     */
+    public function getGroupedPackages() {
+        $grouped = array();
 
-			if (array_key_exists($name, $grouped)) {
-				$grouped[$name][] = $package;
-			} else {
-				$grouped[$name] = array($package);
-			}
-		}
+        foreach ($this->getPackages() as $package) {
+            $name = $package->getName();
 
-		return $grouped;
-	}
+            if (array_key_exists($name, $grouped)) {
+                $grouped[$name][] = $package;
+            } else {
+                $grouped[$name] = array($package);
+            }
+        }
 
-	/**
-	 * Gets detailed information for a package.
-	 *
-	 * @param string $name
-	 * @return array
-	 */
-	public function getPackageDetails($name) {
-		return $this->client->get($name);
-	}
+        return $grouped;
+    }
 
-	/**
-	 * Gets all versions of a package by name.
-	 *
-	 * @param $name
-	 * @return \Composer\Package\PackageInterface[]
-	 */
-	public function getPackageVersions($name) {
-		$packages = array();
-		$loader = new ArrayLoader();
+    /**
+     * Gets detailed information for a package.
+     *
+     * @param string $name
+     * @return array
+     */
+    public function getPackageDetails($name) {
+        return $this->client->get($name);
+    }
 
-		$package = $this->client->get($name);
+    /**
+     * Gets all versions of a package by name.
+     *
+     * @param $name
+     * @return \Composer\Package\PackageInterface[]
+     */
+    public function getPackageVersions($name) {
+        $packages = array();
+        $loader = new ArrayLoader();
 
-		foreach ($package->getVersions() as $repo) {
-			$packages[] = $repo;
-		}
+        $package = $this->client->get($name);
 
-		return $packages;
-	}
+        foreach ($package->getVersions() as $repo) {
+            $packages[] = $repo;
+        }
+
+        return $packages;
+    }
 
 }
