03796a066b37bc7049e0e130fcb2ffdca2f80824
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-10-21 12:09:03 +1300

    FIX: WIP

--- app/src/dataobjects/Addon.php
+++ app/src/dataobjects/Addon.php
@@ -37,14 +37,15 @@ class Addon extends DataObject
         'RatingDetails'     => 'Text',
         // Commercially supported by SilverStripe Ltd.
         'Supported'         => 'Boolean',
+        'Obsolete' => 'Boolean'
     );
 
     private static $has_one = array(
-        'Vendor' => 'AddonVendor',
+        'Vendor' => AddonVendor::class,
     );
 
-    private static $has_many = array(
-        'Versions' => 'AddonVersion'
+    public static $has_many = array(
+        'Versions' => AddonVersion::class,
     );
 
     private static $many_many = array(
@@ -53,12 +54,69 @@ class Addon extends DataObject
         'CompatibleVersions' => SilverStripeVersion::class,
     );
 
-    private static $default_sort = 'Name';
+    public static $indexes = array(
+        'Name' => true,
+        'Obsolete' => true
+    );
 
+    public static $default_sort = 'Name';
+
+    public static $extensions = array(
+        'Symbiote\\Elastica\\Searchable'
+    );
+
+    private $_lastTaggedVersion = null;
+
+    public function LastTaggedVersion()
+    {
+        if (! $this->_lastTaggedVersion) {
+            $excludeArray = array(
+                'Version' => array('dev-master', 'trunk'),
+                'PrettyVersion' => array('dev-master', 'trunk')
+            );
+            $keys = $this->Versions()->exclude($excludeArray)->column('ID');
+            if (count($keys)) {
+                $values = $this->Versions()->exclude($excludeArray)->column('PrettyVersion');
+            } else {
+                $keys = $this->Versions()->column('ID');
+                $values = $this->Versions()->column('PrettyVersion');
+            }
+            $versions = array_combine($keys, $values);
+            uasort($versions, function ($a, $b) {
+                return version_compare($b, $a);
+            });
+            reset($versions);
+            $id = key($versions);
+            $this->_lastTaggedVersion = AddonVersion::get()->byID($id);
+        }
+        return $this->_lastTaggedVersion;
+    }
+
+    public function DownloadsMonthlyIfEditedThisMonth()
+    {
+        $lastTaggedVersion = $this->LastTaggedVersion();
+        $ageInSeconds = time() - strtotime($lastTaggedVersion->Released);
+        if ($ageInSeconds < (86400 * 30)) {
+            return $this->DownloadsMonthly;
+        }
+        return 0;
+    }
+
+    public function IsCurrent()
+    {
+        $currentFrameworkLevel = Config::inst()->get('SilverStripeVersion', 'current_framework_level');
+        $array = explode(',', $this->FrameworkSupportList);
+        foreach($array as $val) {
+            $val = intval($val);
+            if($val >= $currentFrameworkLevel) {
+                return true;
+            }
+        }
+        return false;
+    }
     private static $extensions = [
         Searchable::class,
     ];
-
     /**
      * Gets the addon's versions sorted from newest to oldest.
      *
@@ -73,6 +131,26 @@ class Addon extends DataObject
         });
 
         return new ArrayList($versions);
+    }
+
+    public function getFrameworkSupport()
+    {
+        $al = ArrayList::create();
+        $array = explode(',', $this->FrameworkSupportList);
+        if (is_array($array) && count($array)) {
+            sort($array);
+            $hasConstraints = false;
+            if (count($array)) {
+                foreach ($array as $constraint) {
+                    if (intval($constraint) > 0) {
+                        $hasConstraints = true;
+                        $al->push(ArrayData::create(array('Supports' => $constraint)));
+                    }
+                }
+            }
+        }
+
+        return $al;
     }
 
     /**
@@ -118,7 +196,34 @@ class Addon extends DataObject
 
     public function PackageName()
     {
+        return $this->getPackageName();
+    }
+    public function getPackageName()
+    {
         return substr($this->Name, strpos($this->Name, '/') + 1);
+    }
+    public function getPackageNameNice()
+    {
+        $name = preg_replace('/^'.preg_quote('silverstripe-', '/').'\s*/i', '', $this->getPackageName());
+        $name = str_replace(['-', '_'], [' ', ' '], $name);
+
+        return $name;
+    }
+
+    public function SimpleType()
+    {
+        return $this->getSimpleType();
+    }
+    public function getSimpleType()
+    {
+        switch ($this->Type) {
+            case 'theme':
+                return 'theme';
+            case 'recipe':
+                return 'recipe';
+            default:
+                return 'module';
+        }
     }
 
     public function Link()
@@ -128,6 +233,38 @@ class Addon extends DataObject
             'add-ons',
             $this->Name
         );
+    }
+
+    public function LinkNew()
+    {
+        $nameArray = explode('/', $this->Name);
+        if (isset($nameArray[0]) && $nameArray[0]) {
+            if (isset($nameArray[1]) && $nameArray[1]) {
+                $team = strtolower($nameArray[0]);
+                $title = strtolower($nameArray[1]);
+                return '//ssmods.com/#~(cfi~(Title~(~(vtm~\''.$title.'~ivl~\''.$title.'))~Team~(~(vtm~\''.$team.'~ivl~\''.$team.')))~csr~(sdi~\'desc~sct~\'RD))';
+                return
+                    '/'.
+                    '#~(cfi('.
+                    '~Title~(~(vtm~\''.$title.'~ivl~\''.$title.'))'.
+                    '~Team~(~(vtm~\''.$team.'~ivl~\''.$team.'))'.
+                    '))';
+            }
+        }
+    }
+
+    /**
+     * @return string | null
+     */
+    public function DocLink()
+    {
+        if (
+            file_exists('/var/www/docs.ssmods.com/public_html/'.$this->Name.'/classes.xhtml') ||
+            Director::isdev()
+        ) {
+            $url = '//docs.ssmods.com/'.$this->Name.'/classes.xhtml';
+            return $url;
+        }
     }
 
     public function DescriptionText()
@@ -145,34 +282,44 @@ class Addon extends DataObject
         return "https://packagist.org/packages/$this->Name";
     }
 
+    /**
+     * Remove the effect of code of conduct Helpful Robot measure that we currently don't include in the Supported module definition
+     *
+     * @return integer Adjusted Helpful Robot score
+     */
+    public function getAdjustedHelpfulRobotScore()
+    {
+        return round(min(100, $this->HelpfulRobotScore / 92.9 * 100));
+    }
+
     public function getElasticaMapping()
     {
         return new Mapping(null, array(
-            'name'          => array('type' => 'string'),
-            'description'   => array('type' => 'string'),
-            'type'          => array('type' => 'string'),
+            'name' => array('type' => 'string'),
+            'description' => array('type' => 'string'),
+            'type' => array('type' => 'string'),
             'compatibility' => array('type' => 'string'),
-            'vendor'        => array('type' => 'string'),
-            'tags'          => array('type' => 'string'),
-            'released'      => array('type' => 'date'),
-            'downloads'     => array('type' => 'long'),
-            'readme'        => array('type' => 'string')
+            'vendor' => array('type' => 'string'),
+            'tags' => array('type' => 'string'),
+            'released' => array('type' => 'date'),
+            'downloads' => array('type' => 'string'),
+            'readme' => array('type' => 'string')
         ));
     }
 
     public function getElasticaDocument()
     {
         return new Document($this->ID, array(
-            'name'          => $this->Name,
-            'description'   => $this->Description,
-            'type'          => $this->Type,
+            'name' => $this->Name,
+            'description' => $this->Description,
+            'type' => $this->Type,
             'compatibility' => $this->CompatibleVersions()->column('Name'),
-            'vendor'        => $this->VendorName(),
-            'tags'          => $this->Keywords()->column('Name'),
-            'released'      => $this->obj('Released')->Format('c'),
-            'downloads'     => (int)$this->Downloads,
-            'readme'        => strip_tags($this->Readme),
-            'SS_Published'  => true,
+            'vendor' => $this->VendorName(),
+            'tags' => $this->Keywords()->column('Name'),
+            'released' => $this->obj('Released')->Format('c'),
+            'downloads' => (int) $this->Downloads,
+            'readme' => strip_tags($this->Readme)
+            // '_boost' => sqrt($this->Downloads)
         ));
     }
 
@@ -198,6 +345,47 @@ class Addon extends DataObject
     public function getDateCreated()
     {
         return date('Y-m-d', strtotime($this->Created));
+    }
+
+    /**
+     * @return array
+     */
+    public function HelpfulRobotData()
+    {
+        $data = json_decode($this->HelpfulRobotData, true);
+
+        return new ArrayData($data["inspections"][0]);
+    }
+
+    public function FilteredKeywords()
+    {
+        $list = $this->Keywords()->exclude(
+            array(
+                'Name' => array(
+                    'Silverstripe',
+                    'CMS',
+                    'cms',
+                    'silverstripe',
+                    'silver stripe',
+                    'silvertripe CMS'
+                )
+            )
+        );
+        return $list;
+    }
+
+
+    public function canEdit($member = null)
+    {
+        return parent::canEdit($member);
+    }
+
+    public function canView($member = null)
+    {
+        if (Permission::checkMember($member, "CMS_ACCESS_EDIT_KEYWORDS")) {
+            return true;
+        }
+        return $this->canEdit($member);
     }
 
     /**
