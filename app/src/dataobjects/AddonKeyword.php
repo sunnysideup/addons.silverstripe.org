f0e3cb821f6f3ea31be32fdb3c797de6d01dbc98
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-10-21 12:11:25 +1300

    FIX: WIP

--- app/src/dataobjects/AddonKeyword.php
+++ app/src/dataobjects/AddonKeyword.php
@@ -13,8 +13,23 @@ class AddonKeyword extends DataObject
     );
 
     private static $belongs_many_many = array(
-        'Addons' => 'Addon',
-        'Versions' => 'AddonVersion'
+        'Addons' => Addon::class,
+        'Versions' => AddonVersion::class,
+        'ExtensionTagGroups' => ExtensionTagGroup::class
+    );
+
+    private static $summary_fields = array(
+        'Name' => 'Name',
+        'AddonCount' => 'Addons',
+        'GroupCount' => 'Groups',
+        'Ignore.Nice' => 'Ignore'
+    );
+
+    private static $searchable_fields = array(
+        'Name' => 'PartialMatchFilter',
+        'AddonCount' => 'PartialMatchFilter',
+        'GroupCount' => 'PartialMatchFilter',
+        'Ignore' => 'PartialMatchFilter'
     );
 
     /**
@@ -35,5 +50,40 @@ class AddonKeyword extends DataObject
         }
 
         return $kw;
+    }
+
+    /**
+     * Event handler called before writing to the database.
+     */
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        if ($this->exists()) {
+            $this->AddonCount = $this->Addons()->count();
+            $this->GroupCount = $this->ExtensionTagGroups()->count();
+        }
+    }
+
+    public function canCreate($member = null)
+    {
+        return $this->canEdit($member);
+    }
+
+    public function canView($member = null)
+    {
+        return $this->canEdit($member);
+    }
+
+    public function canEdit($member = null)
+    {
+        if (Permission::checkMember($member, "CMS_ACCESS_EDIT_KEYWORDS")) {
+            return true;
+        }
+        return parent::canEdit($member);
+    }
+
+    public function canDelete($member = null)
+    {
+        return $this->Addons()->count() > 0 ? false : $this->canEdit($member);
     }
 }
